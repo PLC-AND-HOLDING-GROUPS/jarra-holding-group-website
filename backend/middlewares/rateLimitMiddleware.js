d38b33f56daf5efa96c50b0552def2d063a0dbfb
AbeDevSaga <2000amandanm@gmail.com>
2026-09-15 11:06:30 +0300

    feat: add rate limiting middleware for backend endpoints

--- backend/middlewares/rateLimitMiddleware.js
+++ backend/middlewares/rateLimitMiddleware.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const rateLimit = require("express-rate-limit");
+const jwt = require("jsonwebtoken");
 
 // General settings for error responses
 const handler = (req, res, next, options) => {
@@ -10,6 +11,20 @@ const handler = (req, res, next, options) => {
     });
 };
 
+const skipIfAuthenticated = (req, res) => {
+    const authHeader = req.headers["x-authorization"] || req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token) {
+        try {
+            jwt.verify(token, process.env.JWT_SECRET);
+            return true;
+        } catch (err) {
+            return false;
+        }
+    }
+    return false;
+};
+
 // 1. Contact Submissions (10 req / 15 min / IP)
 exports.contactLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
@@ -17,7 +32,8 @@ exports.contactLimiter = rateLimit({
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many contact messages from this IP, please try again after 15 minutes.",
-    handler
+    handler,
+    skip: skipIfAuthenticated
 });
 
 // 2. Product Inquiries (10 req / 15 min / IP)
@@ -27,7 +43,8 @@ exports.productInquiryLimiter = rateLimit({
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many product inquiries from this IP, please try again after 15 minutes.",
-    handler
+    handler,
+    skip: skipIfAuthenticated
 });
 
 // 3. Job Applications (5 req / 1 hour / IP)
@@ -37,7 +54,8 @@ exports.jobApplicationLimiter = rateLimit({
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many job applications from this IP, please try again after an hour.",
-    handler
+    handler,
+    skip: skipIfAuthenticated
 });
 
 // 4. File Uploads (10 req / 1 hour / IP)
@@ -47,7 +65,8 @@ exports.fileUploadLimiter = rateLimit({
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many file uploads from this IP, please try again after an hour.",
-    handler
+    handler,
+    skip: skipIfAuthenticated
 });
 
 // 5. News Interactions (30 req / 15 min / IP)
@@ -57,5 +76,6 @@ exports.newsInteractionLimiter = rateLimit({
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many interactions from this IP, please try again after 15 minutes.",
-    handler
+    handler,
+    skip: skipIfAuthenticated
 });
